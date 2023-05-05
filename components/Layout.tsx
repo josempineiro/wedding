af313cc0fd26ae712b0a7497115f594e2a3b675f
José Manuel Piñeiro García <josempga@ext.inditex.com>
2023-05-05 23:46:30 +0100

    Add Layout and QueryClientProvider to App component

---
+++ components/Layout.tsx
@@ -0,0 +1,37 @@
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import Dashboard from "@/components/Dashboard";
+
+const LogoutButton = () => {
+  const supabase = useSupabaseClient();
+  const logout = async () => {
+    const result = await supabase.auth.signOut();
+    alert(result.error);
+  };
+  return <button onClick={logout}>Logout</button>;
+};
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+  return (
+    <div className="container" style={{ padding: "50px 0 100px 0" }}>
+      {!session ? (
+        <Auth
+          providers={[]}
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          theme="dark"
+        />
+      ) : (
+        <>
+          <LogoutButton />
+          {children}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,39 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from "react";
+import type { AppProps } from "next/app";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Layout from "@/components/Layout";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+});
+
+function App({ Component, pageProps }: AppProps) {
+  const [supabase] = useState(() =>
+    createBrowserSupabaseClient({
+      supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionContextProvider
+        supabaseClient={supabase}
+        initialSession={pageProps.initialSession}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionContextProvider>
+    </QueryClientProvider>
+  );
 }
+
+export default App;
