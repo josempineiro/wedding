0b080b9b140c0a3c8bcecb4d2c14d19765d25ac5
José Manuel Piñeiro García <josempga@ext.inditex.com>
2023-05-06 01:00:37 +0100

    Implement pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,7 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Dashboard from "@/components/wedding/Dashboard";
 
-const inter = Inter({ subsets: ["latin"] });
+const Home = () => {
+  return <Dashboard />;
+};
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1 className="text-4xl font-bold text-center">
-        Welcome to{" "}
-        <a className="text-blue-600" href="https://nextjs.org">
-          Wedding
-        </a>
-      </h1>
-    </main>
-  );
-}
+export default Home;
