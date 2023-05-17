d8a109efd3e25c5a6daa2affdea0d9ebf808c3e3
José Manuel Piñeiro García <josempga@ext.inditex.com>
2023-05-17 09:06:32 +0100

    Add mutation notifications

--- hooks/wedding/use-cases/useAddGuest.ts
+++ hooks/wedding/use-cases/useAddGuest.ts
@@ -1,16 +1,23 @@
 import { Guest } from "@/domain/wedding/entities/Guest";
-import {
-  useMutationUseCase,
-  UseMutationUseCaseOptions,
-} from "@/hooks/core/use-cases/useMutationUseCase";
+import { useMutationUseCaseWithNotifications } from "@/hooks/core/use-cases/useMutationUseCaseWithNotifications";
 import { useWeddingApplication } from "@/hooks/wedding/application/useWeddingApplication";
+import { UseMutationUseCaseOptions } from "@/hooks/core/use-cases/useMutationUseCase";
 
 export function useAddGuest(
   options?: UseMutationUseCaseOptions<Guest, Guest, Array<Guest>>
 ) {
   const weddingApplication = useWeddingApplication();
-  return useMutationUseCase<Guest, Guest, Array<Guest>>({
+  return useMutationUseCaseWithNotifications<Guest, Guest, Array<Guest>>({
     useCase: weddingApplication.domain.useCases.addGuest,
-    options,
+    options: {
+      ...options,
+      notification: (guest, level, results) => {
+        return {
+          id: `${guest.id}-add-guest`,
+          level,
+          message: `${guest.name} added! 🎉`,
+        };
+      },
+    },
   });
 }
--- hooks/wedding/use-cases/useDeleteGuests.ts
+++ hooks/wedding/use-cases/useDeleteGuests.ts
@@ -1,10 +1,30 @@
 import { Guest } from "@/domain/wedding/entities/Guest";
-import { useMutationUseCase } from "@/hooks/core/use-cases/useMutationUseCase";
+import { useMutationUseCaseWithNotifications } from "@/hooks/core/use-cases/useMutationUseCaseWithNotifications";
 import { useWeddingApplication } from "@/hooks/wedding/application/useWeddingApplication";
+import { UseMutationUseCaseOptions } from "@/hooks/core/use-cases/useMutationUseCase";
 
-export function useDeleteGuests() {
+export function useDeleteGuests(
+  options?: UseMutationUseCaseOptions<Array<Guest>, void, Array<Guest>>
+) {
   const weddingApplication = useWeddingApplication();
-  return useMutationUseCase<Array<Guest>, void, Array<Guest>>({
+  return useMutationUseCaseWithNotifications<Array<Guest>, void, Array<Guest>>({
     useCase: weddingApplication.domain.useCases.deleteGuests,
+    options: {
+      ...options,
+      notification: (guests, level) => {
+        if (guests.length === 1) {
+          return {
+            id: `${guests[0].id}-delete-guests`,
+            level,
+            message: `${guests[0].name} was removed from the wedding`,
+          };
+        }
+        return {
+          id: "delete-guests",
+          level,
+          message: `${guests?.length} less seats at the wedding`,
+        };
+      },
+    },
   });
 }
