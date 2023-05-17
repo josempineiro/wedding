61b41a45f2458fdeeb6b257616936f3a55bf8e95
José Manuel Piñeiro García <josempga@ext.inditex.com>
2023-05-17 09:06:05 +0100

    Fix repository save method

--- domain/core/repositories/Repository.ts
+++ domain/core/repositories/Repository.ts
@@ -1,7 +1,7 @@
 import { Entity } from "@/domain/core/entities/Entity";
 
 export interface Repository<T extends Entity> {
-  save(entity: T | T[]): Promise<T>;
+  save(entity: T): Promise<T>;
   saveAll(entities: T[]): Promise<T[]>;
   delete(entities: T[]): Promise<void>;
   findById(id: string): Promise<T | undefined>;
