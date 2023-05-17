71d4f789220fae1cf78e8c6a8649abe0f12416a9
José Manuel Piñeiro García <josempga@ext.inditex.com>
2023-05-17 09:03:30 +0100

    Improve button component

--- components/core/buttons/Button.tsx
+++ components/core/buttons/Button.tsx
@@ -5,7 +5,7 @@ export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   type?: "button" | "submit" | "reset";
   variant?: "primary" | "secondary" | "default";
-  size?: "sm" | "md" | "lg";
+  size?: "xs" | "sm" | "md" | "lg";
   rounded?: boolean;
   disabled?: boolean;
 }
@@ -20,23 +20,29 @@ export const Button = ({
 }: ButtonProps) => {
   return (
     <button
-      {...props}
+      role="button"
       className={classNames([
         className,
         "flex flex-row gap-4 justify-center items-center transition-colors duration-200 ease-in-out",
         {
-          "bg-primary-400 hover:bg-primary-500": variant === "primary",
-          "text-white ": variant === "primary",
-          "px-2 py-1 text-sm": size === "sm",
-          "px-4 py-2 text-md": size === "md",
-          "px-6 py-3 text-lg": size === "lg",
-          "py-2 h-8 w-8": size === "sm" && rounded,
-          "p-2 h-14 w-14": size === "md" && rounded,
-          "py-6 h-10 w-": size === "lg" && rounded,
+          "bg-primary-400 hover:bg-primary-500 text-white ":
+            variant === "primary",
+          "bg-secondary-400 hover:bg-secondary-500 text-white ":
+            variant === "secondary",
+          "bg-gray-200 hover:bg-gray-300 text-gray-700 ": variant === "default",
+          "px-2 py-1 text-xs": size === "xs",
+          "px-3 py-1.5 text-xs": size === "sm",
+          "px-4 py-2 text-sm": size === "md",
+          "px-6 py-3 text-md": size === "lg",
+          "px-1 h-4 w-4": size === "xs" && rounded,
+          "px-1.5 h-5 w-5": size === "sm" && rounded,
+          "px-2 h-8 w-8": size === "md" && rounded,
+          "px-3 h-12 w-12": size === "lg" && rounded,
           "rounded-full ": rounded,
-          "rounded-md": !rounded,
+          "rounded-sm": !rounded,
         },
       ])}
+      {...props}
     >
       {children}
     </button>
